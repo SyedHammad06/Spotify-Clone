2613dde93407801df5db4e9fc5df6d05c8eecade
dhanushsurendra <dhanushsurendra172@gmail.com>
2021-01-08 19:06:50 +0530

    Finished with redux basic setup.

---
+++ axios-firebase.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const instance = axios.instance({
+    baseUrl: ''
+});
+
+export default instance;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { BrowserRouter } from "react-router-dom";
+
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+
+import { Provider } from "react-redux";
+
+import thunk from "redux-thunk";
+
+const composeEnhacers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer, composeEnhacers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
