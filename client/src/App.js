a04952c030fabd3a1615ef3f1d242eaf18576fd7
SyedHammad06 <58735133+SyedHammad06@users.noreply.github.com>
2021-01-10 00:44:42 +0530

    react changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,23 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={apiText:""};
-  }
+const App = () => {
+  const [apiText, setApiText] = useState('');
 
-  apiReq(){
-    fetch("http://localhost:8080/api")
-    .then(res=>res.text())
-    .then(res=>this.setState({apiText:res}))
-    .catch(err=>console.log(err))
-  }
+  const apiReq = () => {
+    fetch('http://localhost:8080/api')
+      .then((res) => res.text())
+      .then((res) => setApiText(res))
+      .catch((err) => console.log(err));
+  };
 
-  componentDidMount(){
-    this.apiReq()
-  }
+  useEffect(() => {
+    apiReq();
+  }, []);
 
-  render() { 
-    return ( 
-      <div>
-        {/* currently this is a test api, will reconfigure depending later on the json context and as the project further improvises */}
-        <p>{this.state.apiText}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <p>{apiText}</p>
+    </div>
+  );
+};
 export default App;
-
