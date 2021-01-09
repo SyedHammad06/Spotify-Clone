601a6709a9df686db07453e3c5887c470ddadb89
SyedHammad06 <58735133+SyedHammad06@users.noreply.github.com>
2021-01-10 01:03:55 +0530

    final commit day 1

--- client/src/index.js
+++ client/src/index.js
@@ -3,25 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter } from 'react-router-dom';
-
-// import { combineReducers, createStore, applyMiddleware, compose } from "redux";
-
-// import { Provider } from "react-redux";
-
-// import thunk from "redux-thunk";
-
-// const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
-// const rootReducer = combineReducers({});
-
-// const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
