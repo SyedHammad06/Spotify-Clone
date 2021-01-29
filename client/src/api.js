d63ed5b1a1a6cc96927150708e3ac6fffe729e90
Saif Khan <Saif50172169@users.noreply.github.com>
2021-01-29 14:31:14 +0530

    api working

--- client/src/api.js
+++ client/src/api.js
@@ -4,11 +4,12 @@ class myComponent extends React.Component {
   componentDidMount() {
     const apiUrl = 'http://localhost:8080/api';
     fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log('This is your data', data));
-  }
-  render() {
-    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
-  }
+    .then((response) => response.json())
+    .then((data) => console.log('This is your data', data))
+    .catch(err=>console.log(err));
+}
+    render() {
+        return <h1>my Component has Mounted, Check the browser 'console' </h1>;
+    }
 }
 export default myComponent;
